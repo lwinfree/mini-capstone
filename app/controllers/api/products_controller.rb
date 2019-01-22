2bd859cdbd0b476d454b2cd555178a42fc21289e
lilly <lillywinfree@gmail.com>
2019-01-21 17:31:07 -0800

    adds restful API routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,28 +1,73 @@
 class Api::ProductsController < ApplicationController
 
-  def all_products_method
+  # def all_products_method
+  #   @products = Product.all
+  #   render 'all-products.json.jbuilder'
+  # end
+
+  # def canvas_method
+  #   @canvas = Product.find_by(name: "canvas")
+  #   render 'canvas.json.jbuilder'
+  # end
+
+  # def fan_brush_method
+  #   @fan_brush = Product.find_by(name: "fan brush")
+  #   render 'fan-brush.json.jbuilder'
+  # end
+
+  # def phthalo_blue_method
+  #   @phthalo_blue = Product.find_by(name: "phthalo blue")
+  #   render 'phthalo-blue.json.jbuilder'
+  # end
+
+  # def palette_knife_method
+  #   @palette_knife = Product.find_by(name: "palette knife")
+  #   render 'palette-knife.json.jbuilder'
+  # end
+
+  def query_products
+    query_products = params["product"]
+    # @display = Product.find_by(name:query_products)
+    @display = Product.find_by(id: query_products)
+    render 'query_products.json.jbuilder'
+    #for names with spaces, use %20 for the space in the URL
+  end
+
+  def index
     @products = Product.all
-    render 'all-products.json.jbuilder'
+    render 'index.json.jbuilder'
   end
 
-  def canvas_method
-    @canvas = Product.find_by(name: "canvas")
-    render 'canvas.json.jbuilder'
+  def show
+    @product = Product.find_by(id: params["id"])
+    render 'show.json.jbuilder'
   end
 
-  def fan_brush_method
-    @fan_brush = Product.find_by(name: "fan brush")
-    render 'fan-brush.json.jbuilder'
+  def create
+    @product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"]
+      )
+    @product.save
+    render 'show.json.jbuilder'
   end
 
-  def phthalo_blue_method
-    @phthalo_blue = Product.find_by(name: "phthalo blue")
-    render 'phthalo-blue.json.jbuilder'
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.image_url = params["image_url"] || @product.image_url
+    @product.description = params["description"] || @product.description
+    @product.save
+    render 'show.json.jbuilder'
   end
 
-  def palette_knife_method
-    @palette_knife = Product.find_by(name: "palette knife")
-    render 'palette-knife.json.jbuilder'
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: {message: "destroyed"}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,39 @@ Rails.application.routes.draw do
   
   namespace :api do
     
-    get "/products" => "products#all_products_method"
+    #can replace these commented out routes with the RESTful routes below
+    # get "/products" => "products#all_products_method"
 
-    get "/canvas" => "products#canvas_method"
+    # get "/canvas" => "products#canvas_method"
 
-    get "/fan_brush" => "products#fan_brush_method"
+    # get "/fan_brush" => "products#fan_brush_method"
 
-    get "/phthalo_blue" => "products#phthalo_blue_method"
+    # get "/phthalo_blue" => "products#phthalo_blue_method"
 
-    get "/palette_knife" => "products#palette_knife_method"
+    # get "/palette_knife" => "products#palette_knife_method"
+
+    #create a single route that can display ANY single product using a query parameter
+
+    get "/query_products" => "products#query_products"
+
+    #create a single route that can display ANY single product using a url segment parameter.
+    get "/query_products/:product" => "products#query_products"
+
+    #restful index to list all products
+    get "/products" => "products#index"
+
+    #restful show action to list single products
+    get "/products/:id" => "products#show"
+
+    #RESTful create action. Do this post in Insomnia
+    post "/products" => "products#create"
+
+    #RESTful update action. Run in insomnia
+    patch "/products/:id" => "products#update"
+
+    #RESTful destroy action. Run in insomnia
+    delete "/products/:id" => "products#destroy"
+
 
   end
 
